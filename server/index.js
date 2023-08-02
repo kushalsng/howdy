555871c456765f49b9e07820326785e0e73d0458
Kushal <singhkushal635@gmail.com>
2023-08-02 23:19:37 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { chats } = require("./data");
+require('dotenv').config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("App is running")
+})
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find(chat => chat._id === req.params.id)
+  res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(process.env.PORT, console.log("Server started on port ", PORT));
