be34b549c9e753363d2f3fc3a645170b7238b7d4
Kushal <singhkushal635@gmail.com>
2023-09-10 01:26:32 +0530

    removed Footer from drawer component

--- client/src/Components/Chat/Header.js
+++ client/src/Components/Chat/Header.js
@@ -182,12 +182,6 @@ const Header = () => {
             {loadingChat && <Spinner ml="auto" display="flex" justifyContent="center" />}
           </DrawerBody>
 
-          <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme='blue'>Save</Button>
-          </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </>
