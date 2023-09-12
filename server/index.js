f455a71dbfc67de08c5441eea95beac1e84a956f
Kushal <singhkushal635@gmail.com>
2023-09-12 23:33:32 +0530

    socket cors origin fix

--- server/index.js
+++ server/index.js
@@ -53,7 +53,8 @@ const server = app.listen(
 const io = socket(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://howdy-rvua.onrender.com/',
+    // origin: 'http://localhost:3000',
   },
 });
 
