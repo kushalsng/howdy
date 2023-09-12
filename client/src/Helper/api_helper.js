d76b2aaefc7fd78adb172bd2d0fe0910db83b7c1
Kushal <singhkushal635@gmail.com>
2023-09-12 21:51:48 +0530

    baseURL issue fixed

--- client/src/Helper/api_helper.js
+++ client/src/Helper/api_helper.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = 'https://howdy-rvua.onrender.com/';
+const baseURL = 'https://howdy-rvua.onrender.com';
 const token = localStorage.getItem("token")
 
 axios.interceptors.request.use((request) => {
